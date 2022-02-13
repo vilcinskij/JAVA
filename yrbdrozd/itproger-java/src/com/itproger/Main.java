1326d5a329d0b9c675e6508fdf4b9595ac9b710f
vilcinskij <vilcinskij@gmail.com>
2022-02-13 13:20:01 +0200

    Hello world !

--- yrbdrozd/itproger-java/src/com/itproger/Main.java
+++ yrbdrozd/itproger-java/src/com/itproger/Main.java
@@ -3,7 +3,9 @@ package com.itproger;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.print("Hello world");
+        System.out.print("Hello");
+        System.out.print("world");
+        System.out.print("!");
     }
 
 }
