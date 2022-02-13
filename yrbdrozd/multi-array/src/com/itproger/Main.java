00c2d66fee238c3b64f7335dcfc54ba454402687
vilcinskij <vilcinskij@gmail.com>
2022-02-13 20:46:06 +0200

    multi array

--- yrbdrozd/multi-array/src/com/itproger/Main.java
+++ yrbdrozd/multi-array/src/com/itproger/Main.java
@@ -5,6 +5,17 @@ public class Main {
     public static void main(String[] args) {
 
         char[][] syms = new char[2][2];
+        syms[0][0] = 't';
+        System.out.println(syms[0][0]);
+
+        byte[][] nums = new byte [][] {
+                {5, 7},
+                {7, 3},
+                {2, 0}
+        };
+
+        nums[1][0] = 67;
+        System.out.println(nums[1][0]);
 
     }
 }
