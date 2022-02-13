2e614ade729835e546ae174dcd3b4ef0766a09b6
vilcinskij <vilcinskij@gmail.com>
2022-02-13 19:12:04 +0200

    while

--- yrbdrozd/ciklai/src/com/itproger/Main.java
+++ yrbdrozd/ciklai/src/com/itproger/Main.java
@@ -3,8 +3,14 @@ package com.itproger;
 public class Main {
 
     public static void main(String[] args) {
-	    for(float i = 100; i > 10; i /= 2) {
-            System.out.println("Elenet" + i);
+//	    for(float i = 100; i > 0; i /= 2) {
+//            System.out.println("Elenet: " + i);
+//        }
+
+        int i = 0;
+        while (i < 10) {
+            i++;
+            System.out.println("Element: " + i);
         }
     }
 }
