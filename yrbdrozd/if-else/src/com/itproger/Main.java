50adb802c873ec489b2bb8582006cc82c5b286df
vilcinskij <vilcinskij@gmail.com>
2022-02-13 15:48:31 +0200

    System.out.println("A daugiau nei b")

--- yrbdrozd/if-else/src/com/itproger/Main.java
+++ yrbdrozd/if-else/src/com/itproger/Main.java
@@ -3,6 +3,10 @@ package com.itproger;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int a = 5, b = 10;
+        if (a > b) {
+            System.out.println("A daugiau nei b");
+        }
+        System.out.println("A daugiau nei bsss");
     }
 }
