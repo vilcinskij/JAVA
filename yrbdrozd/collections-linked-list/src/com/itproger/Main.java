caad3c71fef5b064cc82547759b1b0ec4c065bed
vilcinskij <vilcinskij@gmail.com>
2022-02-13 22:04:17 +0200

    linked list

--- yrbdrozd/collections-linked-list/src/com/itproger/Main.java
+++ yrbdrozd/collections-linked-list/src/com/itproger/Main.java
@@ -1,8 +1,19 @@
 package com.itproger;
 
+import java.util.LinkedList;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        LinkedList<Float> numbers = new LinkedList<>();         //Beveik tas pats kas ArrayList, tik greitesnis
+        numbers.add(5.6f);
+        numbers.add(15.48484f);
+        numbers.add(5.6544848f);
+        numbers.add(2f);
+
+        for(Float i: numbers)                                   //Ciklas kolekcijos elementams perrinlti ('DuomenuTipas' 'laikinasKintamasis': 'kolekcijos pavadinimas'
+            System.out.println(i);
+
     }
 }
