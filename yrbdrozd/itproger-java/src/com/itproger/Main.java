81d349fdd743157f2f56a4df08ceba1f3e2d610f
vilcinskij <vilcinskij@gmail.com>
2022-02-13 15:04:26 +0200

    helllo name

--- yrbdrozd/itproger-java/src/com/itproger/Main.java
+++ yrbdrozd/itproger-java/src/com/itproger/Main.java
@@ -5,6 +5,10 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Įveskite savo varda");
+        String username = scan.nextLine();
+        System.out.println("Hello, " + username);
     }
 
 }
