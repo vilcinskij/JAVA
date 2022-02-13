33037058cc5cbfdf3244a6fba63708ee204dc1ae
vilcinskij <vilcinskij@gmail.com>
2022-02-13 21:56:01 +0200

    .add .remove

--- yrbdrozd/collections/src/com/itproger/Main.java
+++ yrbdrozd/collections/src/com/itproger/Main.java
@@ -9,9 +9,12 @@ public class Main {
         ArrayList<Integer> numbers = new ArrayList<>();
         numbers.add(5);
         numbers.add(50);
-        numbers.add(1, 30);
+        numbers.add(1, 30);         //elemento pridėjimas kaip "push". INDEX ir ELEMENT atsiranda AUTOMATIŠKAI!
 
 //        System.out.println("Kolekcijos ilgis: " + numbers.size());
+        System.out.println(numbers.get(1));
+
+        numbers.remove(1);                  //Elemento šalinimas pagal indekso numerį
 
         for(Integer i : numbers) {
             System.out.println(i);
