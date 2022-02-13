b8c3a55fde31589a03994aa0969a2f9b4c3dcb41
vilcinskij <vilcinskij@gmail.com>
2022-02-13 18:02:20 +0200

    .equals

--- yrbdrozd/if-else/src/com/itproger/Main.java
+++ yrbdrozd/if-else/src/com/itproger/Main.java
@@ -1,12 +1,18 @@
 package com.itproger;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        int a = 5, b = 10;
-        if (a > b) {
-            System.out.println("A daugiau nei b");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Įveskite vartotojo tipą");
+        String role = scanner.nextLine();
+        if (role.equals("Admin")) {
+//            .equals naudojama tik su String, vietoj ==
+            System.out.println("Visi vartotojai");
+        } else {
+            System.out.println("Sveikas, koks tavo vardas?");
         }
-        System.out.println("A daugiau nei bsss");
     }
 }
