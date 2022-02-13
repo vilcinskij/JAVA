04597dfc85143d46a5fe19a992dfd87a71ede567
vilcinskij <vilcinskij@gmail.com>
2022-02-13 13:35:49 +0200

    kintamieji

--- yrbdrozd/itproger-java/src/com/itproger/Main.java
+++ yrbdrozd/itproger-java/src/com/itproger/Main.java
@@ -3,8 +3,10 @@ package com.itproger;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello\n\tworld!");
-            //        pirma programa
+        System.out.println("Duomenys");
+
+        int age;
+        age = 56;
     }
 
 }
