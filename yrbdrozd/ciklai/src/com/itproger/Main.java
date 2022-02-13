7cd388976476013583b89b5bb8dbdad8205aedff
vilcinskij <vilcinskij@gmail.com>
2022-02-13 19:20:52 +0200

    for if

--- yrbdrozd/ciklai/src/com/itproger/Main.java
+++ yrbdrozd/ciklai/src/com/itproger/Main.java
@@ -3,9 +3,14 @@ package com.itproger;
 public class Main {
 
     public static void main(String[] args) {
-//	    for(float i = 100; i > 0; i /= 2) {
-//            System.out.println("Elenet: " + i);
-//      }
+	    for(float i = 5; i < 25; i += 2) {
+            if (i % 3 == 0)
+                continue;
+
+            if (i >= 17)
+                break;
+            System.out.println("Elenet: " + i);
+      }
 
 //      int i = 0;
 //      while (i < 10) {
@@ -13,10 +18,10 @@ public class Main {
 //          System.out.println("Element: " + i);
 //      }
 
-        int i = 100;
-        do {
-            i *= 10;
-            System.out.println("Element: " + i);
-        } while(i < 10);
+//        int i = 100;
+//        do {
+//            System.out.println("Element: " + i);
+//            i *= 10;
+//        } while(i < 10);
     }
 }
