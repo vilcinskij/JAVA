8601cd3b2806eb334703e1e7235cad5ba9ddfa8d
vilcinskij <vilcinskij@gmail.com>
2022-02-13 22:20:01 +0200

    hello Java

--- yrbdrozd/funkcijos/src/com/itproger/Main.java
+++ yrbdrozd/funkcijos/src/com/itproger/Main.java
@@ -5,8 +5,9 @@ public class Main {
     public static void main(String[] args) {
 
         info( "Hello");                                                     //'word' ATSIRANDA AUTOMATIŠKAI!!!
-        info();
-        info();
+        String java = "Java";
+        info(java);
+        info("");
 
     }
 
