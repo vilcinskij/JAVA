379af86281322173798388ae4b77677512a0b472
vilcinskij <vilcinskij@gmail.com>
2022-02-13 22:14:35 +0200

    hello

--- yrbdrozd/funkcijos/src/com/itproger/Main.java
+++ yrbdrozd/funkcijos/src/com/itproger/Main.java
@@ -3,6 +3,14 @@ package com.itproger;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        info();
+        info();
+        info();
+
+    }
+
+    public static void info() {                                        //info - betkoks pavadinimias
+        System.out.println("Hello");
     }
 }
