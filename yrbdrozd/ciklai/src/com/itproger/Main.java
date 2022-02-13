71d09acbc5ca28d8b3566d66a968fa41f957eeb8
vilcinskij <vilcinskij@gmail.com>
2022-02-13 19:07:07 +0200

    ciklai

--- yrbdrozd/ciklai/src/com/itproger/Main.java
+++ yrbdrozd/ciklai/src/com/itproger/Main.java
@@ -3,7 +3,7 @@ package com.itproger;
 public class Main {
 
     public static void main(String[] args) {
-	    for(int i = 0; i <= 10; i++) {
+	    for(float i = 100; i > 10; i /= 2) {
             System.out.println("Elenet" + i);
         }
     }
