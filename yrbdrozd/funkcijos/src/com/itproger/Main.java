b5a16228670fd12a73e02cbfbef63005d9ac61f5
vilcinskij <vilcinskij@gmail.com>
2022-02-13 23:22:19 +0200

    String.valueOf

--- yrbdrozd/funkcijos/src/com/itproger/Main.java
+++ yrbdrozd/funkcijos/src/com/itproger/Main.java
@@ -3,20 +3,26 @@ package com.itproger;
 public class Main {
 
     public static void main(String[] args) {
+//
+//        info( "Hello");                                                     //'word' ATSIRANDA AUTOMATIŠKAI!!!
+//        String java = "Java";
+//        info(java);
+//        info("");
 
-        info( "Hello");                                                     //'word' ATSIRANDA AUTOMATIŠKAI!!!
-        String java = "Java";
-        info(java);
-        info("");
+        int num1 = 7;
+        int rezult1 = summa(5, num1);
+        info(String.valueOf(rezult1));
 
-        int num = 7;
-        summa(5, num);                                                      // "a:" ir "b:" ATSIRANDA AUTOMATIŠKAI!!!
+        int num2 = 7;
+        int rezult2 = summa(5, num2);                                                      // "a:" ir "b:" ATSIRANDA AUTOMATIŠKAI!!!
 
     }
 
-    public static void summa(int a, int b) {                                  //short, arba int, arba byte....
+    public static int summa(int a, int b) {                                  //short, arba int, arba byte....
         int rez = a + b;
-        System.out.println("Rezultatas: " + rez);
+        String rezult = "Rezultatas: " + rez;
+        info(rezult);
+        return rez;
     }
 
     public static void info(String word) {                                        //info - betkoks pavadinimias, word - betkoks pavadinimias
