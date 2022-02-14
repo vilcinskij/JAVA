bd1c30d232062f7833f9504751928d6714777afd
vilcinskij <vilcinskij@gmail.com>
2022-02-14 02:08:53 +0200

    finn class

--- yrbdrozd/class/src/com/itproger/Main.java
+++ yrbdrozd/class/src/com/itproger/Main.java
@@ -5,15 +5,21 @@ public class Main {
     public static void main(String[] args) {
 
         Transport bmw = new Transport();
-        bmw.speed = 250.5f;
-        bmw.weight = 2500;
-        bmw.color = "White";
-        bmw.coordinste = new byte[] {0, 0, 0};
+        bmw.setValues(250.5f, 2500, "White", new byte[] {0, 0, 0});
+        String res1 = bmw.getValues();
+        System.out.println(res1);
+//        bmw.speed = 250.5f;
+//        bmw.weight = 2500;
+//        bmw.color = "White";
+//        bmw.coordinate = new byte[] {0, 0, 0};
 
         Transport truck = new Transport();
         truck.speed = 140.5f;
         truck.weight = 5600;
         truck.color = "Red";
-        truck.coordinste = new byte[] {100, 0, 100};
+        truck.coordinate = new byte[] {100, 0, 100};
+
+        String res2 = truck.getValues();
+        System.out.println(res2);
     }
 }
